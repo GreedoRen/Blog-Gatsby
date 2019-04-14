fd05aeb4ca283f067ea09f414f2d6e814e3f33b9
GreedoRen <greedo.it@gmail.com>
2019-04-14 14:41:35 +0300

    Install gatsby-links

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>HELLO MAN!!11</h1>
+    <h1>Home Page</h1>
   </Layout>
 )
 
