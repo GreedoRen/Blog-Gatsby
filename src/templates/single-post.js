184fcc87aeaef5a99d4ee9c70236363d93e9add6
GreedoRen <greedo.it@gmail.com>
2019-04-16 19:17:11 +0300

    Add Social link in blog and diqus in post

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -6,12 +6,21 @@ import { Card, CardBody, CardSubtitle, Badge } from 'reactstrap'
 import Img from 'gatsby-image'
 import {slugify} from '../util/utilityFunctions' 
 import authors from '../util/authors'
+import { DiscussionEmbed } from 'disqus-react'
 
 
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
     const post = data.markdownRemark.frontmatter
     const author = authors.find(x => x.name === post.author)    
+    const baseUrl = 'https://gatsbytutorial.co.uk/'
+    const disqusShortname = 'renrengre'
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug
+    }
+
     return (
         <Layout pageTitle={ post.title } postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}>
             <SEO title={post.title} />
@@ -34,6 +43,26 @@ const SinglePost = ({ data }) => {
                             </ul>
                         </CardBody>
                     </Card>
+                    <h3 className="text-center">
+                        Share this post
+                    </h3>
+                    <div className="text-center">
+                        <ul style={{display:"inline-flex"}}>
+                            <li><a href={'https://www.facebook.com/share/share.php?u=' + baseUrl + pageContext.slug} className="facebook mr-4" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-facebook-f fa-2x"></i>
+                            </a></li>
+                            <li><a href={'https://twitter.com/share/share?url=' + baseUrl + pageContext.slug + '&text=' + post.title + '&via' + 'twitterHandle'} className="twitter mr-4" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-twitter fa-2x"></i>
+                            </a></li>
+                            <li><a href={'https://plus.google.com/' + baseUrl + pageContext.slug} className="google mr-4" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-google fa-2x"></i>
+                            </a></li>
+                            <li><a href={'https://www.linkedin.com/share/shareArticle?url=' + baseUrl + pageContext.slug} className="linkedin mr-4" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-linkedin fa-2x"></i>
+                            </a></li>
+                        </ul>
+                    </div>
+                    <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
         </Layout>
     )
 }
